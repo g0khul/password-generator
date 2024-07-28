ae6e24a67fecd60851db8c0cf9bfd15418628232
gokhul <gokhul.tg@galvanizeme.com>
2024-07-28 21:09:06 +0530

    implemented logic for generating password

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { isNull } from 'node:util';
 
 @Component({
   selector: 'app-root',
@@ -9,11 +10,63 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
+  length = 0;
   password = '';
+  useLetters = false;
+  useSymbols = false;
+  useNumbers = false;
 
-  onButtonClick() {
-    this.password = 'Hey there!!!';
-    console.log('Button is clicked');
+  onChangeLength(event: Event) {
+    const eventValue = event.target as HTMLInputElement;
+    const parsedLength = parseInt(eventValue.value);
+
+    if (!isNaN(parsedLength)) {
+      this.length = parsedLength;
+      console.log(`Length : ${this.length}`);
+    }
+  }
+
+  onChangeUseLetters() {
+    this.useLetters = !this.useLetters;
+    console.log(this.useLetters);
+  }
+
+  onChangeUseSymbols() {
+    this.useSymbols = !this.useSymbols;
+    console.log(this.useSymbols);
+  }
+
+  onChangeUseNumbers() {
+    this.useNumbers = !this.useNumbers;
+    console.log(this.useNumbers);
+  }
+
+  onGenerateButtonClick() {
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const symbols = '!@#$%^&*()';
+
+    let passwordPool = '';
+
+    if (this.useLetters) {
+      passwordPool += letters;
+    }
+
+    if (this.useSymbols) {
+      passwordPool += symbols;
+    }
+
+    if (this.useNumbers) {
+      passwordPool += numbers;
+    }
+
+    let generatedPassword = '';
+    for (let index = 0; index < this.length; index++) {
+      const randomIndex = Math.floor(Math.random() * passwordPool.length);
+      generatedPassword += passwordPool.charAt(randomIndex);
+    }
+
+    this.password = generatedPassword;
   }
 
   getPassword() {
